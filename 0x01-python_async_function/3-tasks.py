ae9ac22f9dfe6bfdb0ff3f840c792901fec55131
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2024-04-22 17:46:30 +0200

    task_three

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Async basics
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Tasks
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
